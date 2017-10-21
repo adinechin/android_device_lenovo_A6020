2fe83da745b0508241995ef04b6f96448e75452f
harry8242 <mohamedharis97@gmail.com>
2017-10-21 13:06:51 +0000

    A6020: Disable Sound trigger

--- product/qcom-audio.mk
+++ product/qcom-audio.mk
@@ -18,3 +18,8 @@ PRODUCT_PACKAGES += \
     libqcompostprocbundle \
     tinymix \
     audio.a2dp.default \
+
+PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.broadcastradio@1.0-impl
