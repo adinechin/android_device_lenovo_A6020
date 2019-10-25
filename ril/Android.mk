230edb230e859c38a408fc7dceea6c28fc020be9
adinechin <adedinechin@gmail.com>
2019-10-25 09:17:59 +0200

    First devs about SIM management based on https://review.lineageos.org/q/topic:%2522kiwi-provisioning%2522+
    Change-Id: I7e16b1c2ce9f11776fcf5719fedd86c18239103b

--- ril/Android.mk
+++ ril/Android.mk
@@ -1,12 +1,11 @@
 #
-# Copyright (C) 2015-2016 The CyanogenMod Project
-# Copyright (C) 2017 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,18 +13,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += device/lenovo/A6020/overlay
-
-# Include package config fragments
-include $(LOCAL_PATH)/product/*.mk
-
-$(call inherit-product-if-exists, vendor/lenovo/A6020/A6020-vendor.mk)
-
-# Radio
-PRODUCT_PACKAGES += \
-    qti-telephony-common
-
-PRODUCT_BOOT_JARS += \
-    telephony-ext
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE := qti-telephony-common
+LOCAL_JAVA_LIBRARIES := telephony-common telephony-ext
+LOCAL_SRC_FILES := $(call all-java-files-under,src)
+LOCAL_MODULE_TAGS := optional
+LOCAL_DEX_PREOPT := false
+include $(BUILD_JAVA_LIBRARY)
